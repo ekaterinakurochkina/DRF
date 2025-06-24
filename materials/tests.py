1d6b2fd8e35535d9e78c0c5c7742ebe43ada409d
Katerina Kurochkina <katifedr@mail.ru>
2025-06-24 17:14:47 +0500

    тесты

--- materials/tests.py
+++ materials/tests.py
@@ -1,8 +1,8 @@
-# from django.urls import reverse
-# from rest_framework import status
+from django.urls import reverse
+from rest_framework import status
 from rest_framework.test import APITestCase
-from users.models import User
 
+from users.models import User
 from .models import Lesson, Course
 
 
